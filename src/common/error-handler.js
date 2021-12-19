d85da588dfb9f4b927037f5b9b9409fefb93780f
Natein <natein@gmail.com>
2021-12-19 19:43:17 +0200

    fix: delete js files

--- src/common/error-handler.js
+++
@@ -1,8 +0,0 @@
-function errorHandler(err, reply) {
-  if(err.code) {
-    const {message} = err;
-    return reply.status(err.code).send({ message });
-  } throw err; 
-}
-
-module.exports = { errorHandler };
