74c1589b6d69ad9cc75e6ccfd0eb0bc383b6b28f
Natein <natein@gmail.com>
2022-01-23 15:34:58 +0200

    fix: move user admin creation to migrations

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -1,22 +1,9 @@
 import { createConnection } from 'typeorm';
 import config from '../../ormconfig';
-import { insert } from '../resources/users/user.repository';
-import  User from '../entities/user.model';
-
-export const insertAdmin = async (): Promise<void> => {
-  await insert(
-    new User({
-      name: 'natein',
-      login: 'admin',
-      password: 'admin',
-    })
-  );
-};
 
 export const tryDBConnect = async (callback: () => void ): Promise<void> => {
   try {
     await createConnection(config);
-    await insertAdmin();
     callback();
     process.stdout.write('Successful connection to database\n');
   } catch (error) {
