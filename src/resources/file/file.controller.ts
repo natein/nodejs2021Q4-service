c24ebfe9b306ba1401b2af87b720579c16c01f7b
Natein <natein@gmail.com>
2022-02-06 22:54:41 +0200

    fix: error in file route

--- src/resources/file/file.controller.ts
+++ src/resources/file/file.controller.ts
@@ -1,57 +1,49 @@
-import fs from 'fs';
-import resolvePath from 'resolve-path';
-import {
-  Controller,
-  Get,
-  Post,
-  Param,
-  Res,
-  UploadedFiles,
-  UseInterceptors,
+import { Controller, Get, NotFoundException, Param, Res, StreamableFile
 } from '@nestjs/common';
-import { FilesInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { editFileName, imageFileFilter } from './utils/file-upload.utils';
+import { join } from 'path';
+import { createReadStream } from 'fs';
+import 'dotenv/config';
+import * as fs from 'fs';
+import * as util from 'util';
+import * as stream from 'stream';
 
-interface fileReponse {
-  originalname: string;
-  filename: string;
+@Controller('file')
+export class FileControllerExpress { 
+
+  @Get(':imgpath')
+  seeUploadedFile(@Param('imgpath') image, @Res() res) {
+    return res.sendFile(image, { root: './files' });
+  }
+
 }
 
 @Controller('file')
-export default class FileController {
-  @Post('multiple')
-  @UseInterceptors(
-    FilesInterceptor('image', 20, {
-      storage: diskStorage({
-        destination: './files',
-        filename: editFileName,
-      }),
-      fileFilter: imageFileFilter,
-    }),
-  )
-  async uploadMultipleFiles(
-    @UploadedFiles() files: Array<Express.Multer.File>,
-  ) {
-    const response: fileReponse[] = [];
-    files.forEach((file) => {
-      const fileReponse = {
-        originalname: file.originalname,
-        filename: file.filename,
-      };
-      response.push(fileReponse);
-    });
-    return response;
-  }
+export class FileControllerFastify {
 
-  @Get(':imgpath')
-  seeUploadedFile(@Param('imgpath') image: string, @Res() res) {
-    const stream = fs.createReadStream(resolvePath(image));
-    res.type('text/html').send(stream);
+  @Get(':filename')
+  getFile(@Param('filename') filename: string): StreamableFile {
+    if (!fs.existsSync(join(join(__dirname, '../../files'), filename))) {
+      throw new NotFoundException();
+    }
+    return new StreamableFile(
+      createReadStream(join(join(__dirname, '../../files'), filename)),
+    );
   }
+  async fileFastifySave(_: string, file, filename): Promise<string> {
+    const pipeline = util.promisify(stream.pipeline);
+    const randomName = Array(4)
+      .fill(null)
+      .map(() => Math.round(Math.random() * 16).toString(16))
+      .join('');
 
-  /*  @Get('/:filename')
-  getFile(@Param('filename') filename: string) {
-    return this.fileService.streamFile(filename);
-  } */
+    const filename_randomName = `${randomName}-${filename}`;
+    const writeStream = fs.createWriteStream(
+      `${join(__dirname, '../../files')}/${filename_randomName}`,
+    );
+
+    await pipeline(file, writeStream);
+    console.log(filename_randomName);
+    return filename_randomName;
+  }
+  
 }
