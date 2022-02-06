d8433f83f376ca2d863a7ace5887de6ac9346607
Natein <natein@gmail.com>
2022-02-06 20:04:33 +0200

    feat: delete old project files

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import { PORT } from './common/config';
-import { app } from './app';
-import { tryDBConnect } from './helpers/db';
-
-tryDBConnect(()=> {
-  app.listen(PORT, () =>
-    console.log(`App is running on http://localhost:${PORT}`)
-  );
-});
